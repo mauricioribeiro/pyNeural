4b929feb07d1d46dd3f483a568135a8b396e516a
Mauricio Ribeiro <mauricioribeiro1992@gmail.com>
2015-07-14 23:10:52 -0300

    adding bias variable in example
    until this version, pyNeural assumes that the first input/weight of the
    Neural is about the Bias

--- test-neuron.py
+++ test-neuron.py
@@ -1,11 +1,13 @@
 from neuron import Neuron
 
 myNeuron = Neuron(3,'step')
-myNeuron.setWeights([0,0,0])
+myNeuron.setWeights([0,0,0]) # the first weight is of Bias
 myNeuron.setMaxInteractions(100)
 
-trainingMatrix = [[1,0,0],[1,0,1],[1,1,0],[1,1,1]]
-desiredArray = [1,1,1,0]
+# learning the AND function
+bias = 1
+trainingMatrix = [[bias,0,0],[bias,1,0],[bias,1,1]]
+desiredArray = [0,0,1]
 converged = myNeuron.train(trainingMatrix,desiredArray)
 
 if converged:
@@ -17,6 +19,6 @@ else:
 for i in myNeuron.rangeWeights():
 	print 'W'+str(i)+': '+str(myNeuron.getWeight(i))
 
-myNeuron.setInputs([1,0,0])
+myNeuron.setInputs([bias,0,1])
 print myNeuron.getTrainingInteractions()
-print 'Output: '+str(myNeuron.think())
+print 'Output: '+str(myNeuron.think()) #output desired is 0
